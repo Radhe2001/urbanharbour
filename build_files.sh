38c5cdb4cdaea6aadc5a8560556d6154526e5451
Radhe2001 <radheshyamjha023@gmail.com>
2023-12-19 22:24:17 +0530

    project second commit

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install pillow
-python3.9 -m pip install psycopg2
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py runserver
-echo "BUILD END"
